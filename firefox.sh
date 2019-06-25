55fb286e84a78d20214ee2ee2633b21aa974fd35
avalitan <sgtflux@gmail.com>
2019-06-25 23:40:23 +0200

    Update firefox.sh

--- firefox.sh
+++ firefox.sh
@@ -2,16 +2,16 @@
 
 function usage() {
 	echo "Usage: $0 [cmd]"
-	echo " commands are: bash, firefox, ff, ffremote"
+	echo " commands are: bash, firefox, ff, ffremote, config"
 }
 
-if [ -z $1 ] ; then
+if [[ "a$1" == "a" ]] ; then
 	/opt/firefox/firefox --no-remote
 elif [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]] ; then
 	usage
 	exit 0
 else
-	if ! [[ "$1" =~ ^(bash|firefox|ff|ffremote|config)$ ]] ; then
+	if ! [[ "$1" =~ ^(bash|shell|firefox|ff|ffremote|config)$ ]] ; then
 		echo "Sorry, unknown command $1"
 		usage
 		exit 1
@@ -19,7 +19,7 @@ else
 		/opt/firefox/firefox --no-remote
 	elif [[ "$1" =~ ^(ffremote)$ ]] ; then
 		/opt/firefox/firefox
-	elif [[ "$1" =~ ^(bash)$ ]] ; then
+	elif [[ "$1" =~ ^(bash|shell)$ ]] ; then
 		/bin/bash
 	elif [[ "$1" =~ ^(config)$ ]] ; then
 		itweb-settings
